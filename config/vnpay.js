dadace4789254a3b70c26706f33ade978df173c2
Nguyễn Đức Lộc <272004cp@gmail.com>
2025-01-01 21:55:11 +0700

    tichhopvnpay.

--- config/vnpay.js
+++ config/vnpay.js
@@ -1,6 +1,6 @@
 module.exports = {
-    vnp_TmnCode: "YOUR_TMN_CODE",
-    vnp_HashSecret: "YOUR_HASH_SECRET",
+    vnp_TmnCode: "08UR3HJV",
+    vnp_HashSecret: "3KVX2ICGY9KFXQKZ6CXCLGVXR8LWC7TE",
     vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
     vnp_ReturnUrl: "http://localhost:3000/vnpay_return"
 };
